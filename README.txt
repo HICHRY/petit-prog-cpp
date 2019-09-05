6f527210b8b2b96e76994150b34ce8d132045c29
HICHRY <hichri@mail.com>
2019-09-05 16:54:57 +0200

    Ajout de description du projet et le fichier code source

---
+++ README.txt
@@ -0,0 +1,4 @@
+Description :
+=============
+Petit programme permettant de resoudre une equation du second degre du type: ax²+bx+c=0
+
---
+++ res-equ-2nd-derge.cpp
@@ -0,0 +1,74 @@
+#include <iostream.h>
+#include <stdlib.h>
+using namespace std ;
+float A,B,C,D,x,y,z;
+void main()
+{
+
+/* message de bienvenue */
+
+cout<<"		bienvenue\n";
+cout<<"ceci est un programme permettant de\nresoudre une equation du second degre a une inconnue\n";
+cout<<"AX2+BX+C=0\n";
+
+/* invitation a la saisi */
+
+cout<<"entrer la valeur de A\n";
+cin>>A;
+cout<<"\nentrer la valeur de B\n";
+cin>>B;
+cout<<"\nentrer la valeur de C\n";
+cin>>C;
+cout<<"resoudre:\n";
+cout<<A<<"X2+"<<B<<"X+"<<C<<"=0\n\n\n";
+cout<<"		calcul du discriminant... \n";
+
+/* calcul du discriminant */
+
+D = B * B - 4 * A * C;
+cout<<"le discriminent est egal a:"<<D<<"\n\n\n";
+cout<<"		recherche des solutions...\n\n";
+
+/* choix de du mode de resolution */
+
+if ( A == 0)
+{
+         float w =  ( -  C ) / B;
+         cout<<"la solution est:  "<<w<<"\n";
+         
+         cout<<"au revoir\n";
+	     system("pause");
+         return 0;
+}
+
+if ( D >= 0 )
+	{
+        float d = sqrt(D) ;
+		float z = ( B - d ) / ( 2 * A );
+		float y = ( B + d ) / ( 2 * A );
+        cout<<"les deux solutions sont:          "<<z<<"  et   "<<y<<"\n";
+        
+        cout<<"au revoir\n";
+	    system("pause");
+        return 0;
+	}
+
+if (D == 0)
+	{
+		float x = B / 2 * A;
+		cout<<"la solution est une racine double et de valeur  "<<z<<"\n";
+
+          cout<<"au revoir\n";
+	       system("pause");
+         return 0;
+	}
+
+if (D <= 0)
+	{
+          cout<<"au revoir\n";
+	      system("pause");
+         return 0;
+	}
+
+}
+
